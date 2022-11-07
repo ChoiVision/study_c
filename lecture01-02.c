80ae5b29652a759a7ee21e27f04fd1f5678fbf9c
Joel <loveacaji@gmail.com>
2022-11-08 01:51:44 +0900

    study_c

--- lecture01-02.c
+++ lecture01-02.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
 
 int main()
 {
-    unsigned int i = 0;
+    // unsigned int i = 0b11111111111111111111111111111111;
+    unsigned int u_max = UINT_MAX + 1;
+    printf("%u\n", u_max);
+
+    // i to binary representation
+    char buffer[33];
+    _itoa(u_max, buffer, 2);
+
+    printf("decimal: %u\n", u_max);
+    printf("binary: %s\n", buffer);
     
-    sizeof(unsigned int);
-    sizeof(i);
+    // printf("%u\n", i);
+    // printf("%u\n", u);
+    // printf("%u",sizeof(unsigned int));
+    // printf("%u",sizeof(i));
+
+
 
 }
